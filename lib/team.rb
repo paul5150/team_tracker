3eed21b019c4cc574955b484a59ee7acf2e1c877
paul <pmorin@gmail.com>
2015-06-05 09:51:00 -0700

    change the name of a team method

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,10 @@
 class Team
 
-  define_method(:initialize) do |group|
-    @group = group
+  define_method(:initialize) do |team_name|
+    @team_name = team_name
   end
 
-  define_method(:group) do
-    @group
+  define_method(:team_name) do
+    @team_name
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,13 +1,13 @@
 require('rspec')
 require('team')
-# require('member')
+require('member')
 
 describe(Team) do
 
-  describe("#group") do
+  describe("#team_name") do
     it("returns the name of the team") do
       test_team = Team.new("Patriots")
-      expect(test_team.group()).to(eq("Patriots"))
+      expect(test_team.team_name()).to(eq("Patriots"))
     end
   end
 
